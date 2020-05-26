f5274009318003a40c6968dcc6b7c000e5ad38e5
Calleu Fuzi <calleufuzi@gmail.com>
2020-05-26 20:22:38 -0300

    feat: created front end implementation

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,28 @@ const express = require("express");
 const restful = require("node-restful");
 const server = express();
 const mongoose = restful.mongoose;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://db/mydb");
+
+// Middleware
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(cors());
+
+// ODM
+const Client = restful.model("Client", {
+  name: { type: String, required: true },
+});
+
+// Rest API
+Client.methods(["get", "post", "put", "delete"]);
+Client.updateOptions({ new: true, runValidators: true });
+
+// Routes
+Client.register(server, "/clients");
 
 server.get("/", (req, res, next) => res.send("Backend is Alive"));
 
